3879b9723bfde31e9346f5e05286a4eb5df7e08c
Aleksandr <maestrosanya@yandex.ru>
2017-04-12 12:20:32 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://real:81/admin/');
+define('HTTP_CATALOG', 'http://real:81/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://real:81/admin/');
+define('HTTPS_CATALOG', 'http://real:81/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/real/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/real/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/real/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/real/admin/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/real/admin/view/template/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/real/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/real/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/real/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/real/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/real/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/real/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/real/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'my_shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://real:81/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://real:81/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/real/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/real/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/real/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/real/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/real/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/real/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/real/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/real/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/real/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/real/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/real/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'my_shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
